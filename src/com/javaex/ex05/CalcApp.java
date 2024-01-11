c96e41914333f7da47eecd4599dd7739f90cc610
seed-yoo <lukdown10@naver.com>
2024-01-11 18:04:00 +0900

    수정(전)

--- src/com/javaex/ex05/CalcApp.java
+++ src/com/javaex/ex05/CalcApp.java
@@ -1,8 +1,19 @@
 package com.javaex.ex05;
 
+import java.util.Scanner;
+
 public class CalcApp {
 
     public static void main(String[] args) {
 
+    	Scanner sc = new Scanner(System.in);
+    	String numLine = sc.nextLine();
+    	
+    	
+    	
+    	
+    	
+    	
+    	sc.close();
     }
 }
